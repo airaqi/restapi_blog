b6cc9cbb37a6ded7dd7575e70586a1ea85d2de7a
Ahmed Iraqi <ahmed.iraqi@gmail.com>
2018-06-19 23:16:46 +0200

    Updating test script comments

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
-//posts post data
+#posts post data
 curl -H "Content-Type: application/json" -X POST -d '{"name": "Top 10 ES6 Features", "url":"http://webapplog.com/es6", "text": ""}'  "http://localhost:3000/posts" 
 
-//updates post data at specific id
+#updates post data at specific id
 curl -H 'Content-Type: application/json' -X PUT -d '{"name": "Top 10 ES6 Features Every Developer Must Know", "url":"http://webapplog.com/es6", "text": ""}' "http://localhost:3000/posts/0"
 
-//gets post data
+#gets post data
 curl "http://localhost:3000/posts" 
 
-//deletes post data at specific id
+#deletes post data at specific id
 curl -X DELETE "http://localhost:3000/posts/0" 
 
